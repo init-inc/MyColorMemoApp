6926c30a1bc16cba5ff0fa145a3464aa54b377e4
yamataku29 <tmn.sby001@gmail.com>
2021-03-03 09:51:51 +0900

    No.8-4

---
+++ MyColorMemoApp/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  MyColorMemoApp
+//
+//  Created by Taku Yamada on 2021/03/03.
+//
+
+import Foundation
+import UIKit // UIに関するクラスが格納されたモジュール
+
+// HomeViewControllerにUIViewControllerを「クラス継承」する
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        // このクラスの画面が表示される際に呼び出されるメソッド
+        // 画面の表示・非表示に応じて実行されるメソッドを「ライフサイクルメソッド」と呼ぶ
+    }
+}
