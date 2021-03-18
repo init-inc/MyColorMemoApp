d7178c064ed91e27cd7b638bb61ff1914f14bc95
yamataku29 <tmn.sby001@gmail.com>
2021-03-19 08:27:03 +0900

    No.8-14

--- MyColorMemoApp/MemoDetailViewController.swift
+++ MyColorMemoApp/MemoDetailViewController.swift
@@ -6,12 +6,12 @@
 //
 
 import UIKit
+import RealmSwift
 
 class MemoDetailViewController: UIViewController {
     @IBOutlet weak var textView: UITextView!
     
-    var text: String = ""
-    var recordDate: Date = Date()
+    var memoData = MemoDataModel()
     
     var dateFormat: DateFormatter {
         let dateFormatter = DateFormatter()
@@ -23,16 +23,17 @@ class MemoDetailViewController: UIViewController {
         super.viewDidLoad()
         displayData()
         setDoneButton()
+        textView.delegate = self
     }
     
     func configure(memo: MemoDataModel) {
-        text = memo.text
-        recordDate = memo.recordDate
+        memoData.text = memo.text
+        memoData.recordDate = memo.recordDate
     }
     
     func displayData() {
-        textView.text = text
-        navigationItem.title = dateFormat.string(from: recordDate)
+        textView.text = memoData.text
+        navigationItem.title = dateFormat.string(from: memoData.recordDate)
     }
     
     @objc func tapDoneButton() {
@@ -44,5 +45,21 @@ class MemoDetailViewController: UIViewController {
         let commitButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(tapDoneButton))
         toolBar.items = [commitButton]
         textView.inputAccessoryView = toolBar
+    }
+    
+    func saveData(with text: String) {
+        let realm = try! Realm()
+        try! realm.write {
+            memoData.text = text
+            memoData.recordDate = Date()
+            realm.add(memoData)
+        }
+    }
+}
+
+extension MemoDetailViewController: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        let updatedText = textView.text ?? ""
+        saveData(with: updatedText)
     }
 }
